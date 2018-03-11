5817861c22d4d8b02d7ac8b34fa30c6afc135247
Michael Tanzer <mt.michael97@gmail.com>
2018-03-11 08:29:17 +0000

    Corrected graphs for vol

--- VolatilityPrediction.py
+++ VolatilityPrediction.py
@@ -13,20 +13,24 @@ def modelVolatility(currency):
 
 
 def saveGraphsFromModelAndDataVol(currency, predict_length):
+    dataset = getSTD(currency)[8:]
+    dataset, scaler = normalise(dataset)
+    trainV, testV = splitTrainTest(dataset, 0.90)
+    trainX, trainY, testX, testY = reshape(trainV, testV)
+
     model = load_model(".\\files\\" + currency + ".h5")
 
-    scaler, datasetV, trainX, trainY, testX, testY = getUsefulData(currency)
 
     predictions = predict_sequences_multiple(model, testX[-predict_length:], predict_length)
-    plot = plotStuff(datasetV, model, scaler, trainX, trainY, testX, testY, predictions, predict_length)
+    plot = plotStuff(dataset, model, scaler, trainX, trainY, testX, testY, predictions, predict_length)
 
     lastDate = datetime.datetime.strptime(getLastDate(currency), '%d/%m/%Y')
-    date_list = [lastDate - datetime.timedelta(days=x) for x in range(0, len(datasetV))][::-1]
+    date_list = [lastDate - datetime.timedelta(days=x) for x in range(0, len(dataset))][::-1]
     date_list.extend([lastDate + datetime.timedelta(days=x) for x in range(1, predict_length + 1)])
 
     dates = [date.strftime('%d/%m/%y') for date in date_list]
 
-    plot.xticks(range(len(datasetV) + predict_length)[::(len(datasetV) + predict_length)//6], dates[::(len(datasetV) + predict_length)//6])
+    plot.xticks(range(len(dataset) + predict_length)[::(len(dataset) + predict_length)//6], dates[::(len(dataset) + predict_length)//6])
     plot.xlabel('Day')
     plot.ylabel('Closing Value')
     plot.title(currency[0].upper() + currency[1:].lower() + " Prediction")
